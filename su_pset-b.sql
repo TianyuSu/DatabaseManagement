5449af392a96cd51c3c6b4834b2bbbb03b8a950d
Tianyu Su <47824231+TianyuSu@users.noreply.github.com>
2019-03-30 17:07:20 -0400

    SQL Format Update

--- su_pset-b.sql
+++ su_pset-b.sql
@@ -3,30 +3,30 @@
 select
 *
 from(select
-	    min(bg.geoid10) as geoid10,
-      min(bg.countyfp10) as county,
-      st_union(bg.geom) as geom,
-	    count(pa.*) as num_prop,
-      avg(pa.imp_val/pa.area_sqft) as avg_value,
-      avg(pa.sale_price/pa.area_sqft) as avg_sale_price,
-      min(pa.descr) as descr,
-      concat(bg.geoid10, pa.descr) as GD
+	min(bg.geoid10) as geoid10,
+	min(bg.countyfp10) as county,
+	st_union(bg.geom) as geom,
+	count(pa.*) as num_prop,
+	avg(pa.imp_val/pa.area_sqft) as avg_value,
+	avg(pa.sale_price/pa.area_sqft) as avg_sale_price,
+	min(pa.descr) as descr,
+	concat(bg.geoid10, pa.descr) as GD
      from
 	    somerville.gb_bg_2010 as bg
 	    left join (select
 	                a.id_0 as assess_id,
-                  a.comm_own as comm_own,
-                  a.imp_val as imp_val,
+                 	a.comm_own as comm_own,
+                  	a.imp_val as imp_val,
 	                a.sale_price as sale_price,
-                  a.sale_date as sale_date,
-                  a.sqft as area_sqft,
+                  	a.sale_date as sale_date,
+                  	a.sqft as area_sqft,
 	                a.descr as descr,
 	                p.geom as geom
                 from(select 
 	                    *
                      from  
 	                    somerville.somassess_17 AS assess
-                        inner join somerville.lu_codes as c on assess.pcc = c.pcc
+                            inner join somerville.lu_codes as c on assess.pcc = c.pcc
                      ) AS a
                    inner join somerville.parcels_17 AS p on a.mbl = p.mbl
                 where 
@@ -34,7 +34,7 @@ from(select
      group by
         GD) as pa_bg
 where
-	(descr = 'Single-Family' OR descr = 'Two-Family' OR descr = 'Three-Family' OR descr = 'Apartment, 4-8 Units' OR descr = 'Apartment, 8+ Units' OR descr = 'Condo') 
+  (descr = 'Single-Family' OR descr = 'Two-Family' OR descr = 'Three-Family' OR descr = 'Apartment, 4-8 Units' OR descr = 'Apartment, 8+ Units' OR descr = 'Condo') 
   and avg_value IS NOT NULL;
 
 
@@ -43,7 +43,7 @@ where
 select
     v.geoid10 as geoid10,
     v.geom as geom,
-	v.num_prop as num_prop,
+    v.num_prop as num_prop,
     inc.num_prop_inc as num_prop_inc,
     (CAST(inc.num_prop_inc AS float)*100/CAST(v.num_prop AS float)) as percentage_inc,
     v.avg_value as avg_value,
@@ -60,17 +60,17 @@ from(select
 	        somerville.gb_bg_2010 as bg
 	        left join (select
 	                    a.id_0 as assess_id,
-                        a.comm_own as comm_own,
-                        a.imp_val as imp_val,
+                            a.comm_own as comm_own,
+                            a.imp_val as imp_val,
 	                    a.sale_price as sale_price,
-                        a.sale_date as sale_date,
-                        a.sqft as area_sqft,
+                            a.sale_date as sale_date,
+                            a.sqft as area_sqft,
 	                    a.descr as descr,
 	                    p.geom as geom
                       from(select 
-	                            *
+	                        *
                             from  
-	                            somerville.somassess_17 AS assess
+	                        somerville.somassess_17 AS assess
                                 inner join somerville.lu_codes as c on assess.pcc = c.pcc
                                 ) AS a
                         inner join somerville.parcels_17 AS p on a.mbl = p.mbl
@@ -83,24 +83,24 @@ from(select
      inner join (select
                     *
                 from(select
-	                    min(bg.geoid10) as geoid10,
+	                min(bg.geoid10) as geoid10,
                         st_union(bg.geom) as geom,
-	                    count(pa.*) AS num_prop_inc
+	                count(pa.*) AS num_prop_inc
                     from
-	                    somerville.gb_bg_2010 as bg
+	                somerville.gb_bg_2010 as bg
 	                left join (select
 	                                a.id_0 as assess_id,
-                                    a.comm_own as comm_own,
-                                    a.imp_val as imp_val,
+                                        a.comm_own as comm_own,
+                                        a.imp_val as imp_val,
 	                                a.sale_price as sale_price,
-                                    a.sale_date as sale_date,
-                                    a.sqft as area_sqft,
+                                        a.sale_date as sale_date,
+                                        a.sqft as area_sqft,
 	                                a.descr as descr,
 	                                p.geom as geom
                                 from(select 
-	                                    *
+	                                *
                                     from  
-	                                    somerville.somassess_17 AS assess
+	                                somerville.somassess_17 AS assess
                                         inner join somerville.lu_codes as c on assess.pcc = c.pcc
                                         ) AS a
                                 inner join somerville.parcels_17 AS p on a.mbl = p.mbl
@@ -121,7 +121,7 @@ from (
 select
     v.geoid10 as geoid10,
     v.geom as geom,
-	v.num_prop as num_prop,
+    v.num_prop as num_prop,
     inc.num_prop_inc as num_prop_inc,
     (CAST(inc.num_prop_inc AS float)*100/CAST(v.num_prop AS float)) as percentage_inc,
     v.avg_value as avg_value,
@@ -130,25 +130,25 @@ from(select
         *
      from(select
 	        min(bg.geoid10) as geoid10,
-            st_union(bg.geom) as geom,
+                st_union(bg.geom) as geom,
 	        count(pa.*) AS num_prop,
-            avg(pa.imp_val/pa.area_sqft) as avg_value,
-            avg(pa.sale_price/pa.area_sqft) as avg_sale_price
+                avg(pa.imp_val/pa.area_sqft) as avg_value,
+                avg(pa.sale_price/pa.area_sqft) as avg_sale_price
           from
 	        somerville.gb_bg_2010 as bg
 	        left join (select
 	                    a.id_0 as assess_id,
-                        a.comm_own as comm_own,
-                        a.imp_val as imp_val,
+                            a.comm_own as comm_own,
+                            a.imp_val as imp_val,
 	                    a.sale_price as sale_price,
-                        a.sale_date as sale_date,
-                        a.sqft as area_sqft,
+                            a.sale_date as sale_date,
+                            a.sqft as area_sqft,
 	                    a.descr as descr,
 	                    p.geom as geom
                       from(select 
-	                            *
+	                        *
                             from  
-	                            somerville.somassess_17 AS assess
+	                        somerville.somassess_17 AS assess
                                 inner join somerville.lu_codes as c on assess.pcc = c.pcc
                                 ) AS a
                         inner join somerville.parcels_17 AS p on a.mbl = p.mbl
@@ -162,30 +162,30 @@ from(select
                     *
                 from(select
 	                    min(bg.geoid10) as geoid10,
-                        st_union(bg.geom) as geom,
+                            st_union(bg.geom) as geom,
 	                    count(pa.*) AS num_prop_inc
                     from
 	                    somerville.gb_bg_2010 as bg
-	                left join (select
+	                    left join (select
 	                                a.id_0 as assess_id,
-                                    a.comm_own as comm_own,
-                                    a.imp_val as imp_val,
+                                        a.comm_own as comm_own,
+                                        a.imp_val as imp_val,
 	                                a.sale_price as sale_price,
-                                    a.sale_date as sale_date,
-                                    a.sqft as area_sqft,
+                                        a.sale_date as sale_date,
+                                        a.sqft as area_sqft,
 	                                a.descr as descr,
 	                                p.geom as geom
-                                from(select 
-	                                    *
-                                    from  
-	                                    somerville.somassess_17 AS assess
-                                        inner join somerville.lu_codes as c on assess.pcc = c.pcc
-                                        ) AS a
-                                inner join somerville.parcels_17 AS p on a.mbl = p.mbl
-                                where 
-                                    (a.imp_val IS NOT NULL) AND (a.comm_own LIKE '%LLC%' OR a.comm_own LIKE '%CORPORATION%')) AS pa on ST_Intersects(bg.geom, pa.geom)
-                    group by
-                        geoid10) as pa_bg
+                                	from(select 
+	                                	*
+                                    		from  
+	                                	    somerville.somassess_17 AS assess
+                                        	    inner join somerville.lu_codes as c on assess.pcc = c.pcc
+                                        	) AS a
+                                		inner join somerville.parcels_17 AS p on a.mbl = p.mbl
+                                	where 
+                                    	(a.imp_val IS NOT NULL) AND (a.comm_own LIKE '%LLC%' OR a.comm_own LIKE '%CORPORATION%')) AS pa on ST_Intersects(bg.geom, pa.geom)
+                    			group by
+                        		geoid10) as pa_bg
                 where
 	                pa_bg.num_prop_inc>0) AS inc ON v.geoid10 = inc.geoid10
     ) AS co;
